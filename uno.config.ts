2b4b663e19628008509fe7c0544148d1b697cbe9
Sapon <sapon@MacBookAirSapon.local>
2026-08-06 23:02:30 +0700

    add font-family name Chakra Petch

--- uno.config.ts
+++ uno.config.ts
@@ -2,4 +2,8 @@ import { defineConfig, presetUno } from 'unocss'
 
 export default defineConfig({
   presets: [presetUno()],
+  shortcuts: {
+     'grid-golden': 'grid gap-4 md:grid-cols-[1fr_0.618fr]',
+    'grid-golden-left': 'grid gap-4 md:grid-cols-[0.618fr_1fr]',
+  }
 })
